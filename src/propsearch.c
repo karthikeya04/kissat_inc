204dc78a5a3dcd929cc0c061fe0c54cd876a5753
Kalind Karia <kalind1610@gmail.com>
2023-04-12 11:32:35 +0530

    fix: update prefetch dest from 1 to 0 for head of watchlist

--- src/propsearch.c
+++ src/propsearch.c
@@ -109,7 +109,7 @@ search_propagate(kissat *solver)
         const unsigned not_lit = NOT(lit);  
         watches *watches = &WATCHES(not_lit);
         if(watches->size)
-          __builtin_prefetch(BEGIN_WATCHES(*watches)), 0, 1); // prefetching q for next function call 
+          __builtin_prefetch(BEGIN_WATCHES(*watches), 0, 0); // prefetching q for next function call 
     }
 
     const unsigned lit = PEEK_STACK(solver->trail, solver->propagated);
